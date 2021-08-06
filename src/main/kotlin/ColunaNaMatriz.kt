677d59081b47e53a2d44b0120f9d69e01f71d68e
Oswaldo Paulo <oswaldo.paulo@gmail.com>
2021-08-05 23:12:14 -0300

    init 3

---
+++ src/main/kotlin/ColunaNaMatriz.kt
@@ -0,0 +1,31 @@
+/*
+Neste problema você deve ler um número que indica uma coluna de uma matriz na qual uma operação deve ser realizada, um caractere maiúsculo, indicando a operação que será realizada, e todos os elementos de uma matriz M[12][12]. Em seguida, calcule e mostre a soma ou a média dos elementos que estão na área verde da matriz, conforme for o caso. A imagem abaixo ilustra o caso da entrada do valor 5 para a coluna da matriz, demonstrando os elementos que deverão ser considerados na operação.
+
+Entrada
+A primeira linha de entrada contem um número C (0 ≤ C ≤ 11) indicando a coluna que será considerada para operação. A segunda linha de entrada contém um único caractere Maiúsculo T ('S' ou 'M'), indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto flutuante que compõem a matriz.
+
+Saída
+Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+ */
+
+fun main(args: Array<String>) {
+
+    val C = readLine()!!.toInt()
+    var sum = 0.0
+    val T = readLine()!!.toUpperCase()
+    val M = Array(12) { DoubleArray(12)}
+
+    for (                              ) {
+        for (                          ) {
+            M[i][j] = readLine()!!.toDouble()
+        }
+    }
+    for (k in 0.until(M.size)) {
+        sum += M[k][C]
+    }
+    if (                  ) sum /= M.size
+    println(sum.format(1))
+}
+
+private fun Double.format(digits: Int) = "%.${digits}f"
+    .format(this).replace(',','.')
