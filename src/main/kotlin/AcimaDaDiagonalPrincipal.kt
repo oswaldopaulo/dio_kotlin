74f1d2f91bb5bd6791ea2a3312fed83821f5793e
Oswaldo Paulo <oswaldo.paulo@gmail.com>
2021-08-10 10:40:29 -0300

    ExibindoNumerosPares.kt AcimaDaDiagonalPrincipal.kt SubstituiçãoEmVetorI.kt

---
+++ src/main/kotlin/AcimaDaDiagonalPrincipal.kt
@@ -0,0 +1,34 @@
+/*
+Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. Em seguida,
+calcule e mostre a soma ou a média considerando somente aqueles elementos que estão acima da diagonal principal da matriz,
+conforme ilustrado abaixo (área verde).
+
+
+
+
+Entrada
+A primeira linha de entrada contem um único caractere Maiúsculo O ('S' ou 'M'), indicando a operação (Soma ou Média)
+que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto flutuante que compõem a matriz.
+
+Saída
+Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+ */
+
+fun main(args: Array<String>) {
+    var resultado = 0.0
+    val T = readLine()!!.toUpperCase()
+    val M = Array(12) { DoubleArray(12) }
+
+    for (i in 0..11) {
+        for (j in 0..11) {
+            M[i][j] = readLine()!!.toDouble()
+            if (j>i) resultado += M[i][j]
+        }
+    }
+
+    if(T=="M") resultado /= 66.00
+    println(resultado.format(1))
+}
+
+private fun Double.format(digits: Int) = "%.${digits}f"
+    .format(this).replace(',','.')
---
+++ src/main/kotlin/ExibindoNumerosPares.kt
@@ -0,0 +1,15 @@
+/*
+Desafios
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+Entrada
+Você receberá 1 valor inteiro N, onde N > 0.
+
+Saída
+Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
+ */
+
+fun main(args: Array<String>) {
+    val N = readLine()!!.toInt()
+    for (i in 2.rangeTo(N).step(2)) println(i) // insira o valor correto
+}
